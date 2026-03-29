5cee2b1c944ae7cddc43b86e910f0de996abc176
Magomed Isaev <critikk99@bk.ru>
2026-03-29 16:39:14 +0300

    feat(views): add dedicated autobooking view pages
    - Add AutobookingListView.vue with list, stats, filters, and infinite scroll
    - Add AutobookingCreateView.vue for creating new autobookings
    - Add AutobookingUpdateView.vue for editing existing autobookings
    - Add index.ts for view exports
    - Views include viewReady signaling for skeleton loading

---
+++ apps/frontend/src/views/autobooking/index.ts
@@ -0,0 +1,3 @@
+export { default as AutobookingListView } from './AutobookingListView.vue';
+export { default as AutobookingCreateView } from './AutobookingCreateView.vue';
+export { default as AutobookingUpdateView } from './AutobookingUpdateView.vue';
