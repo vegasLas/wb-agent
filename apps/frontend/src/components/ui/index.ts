60da9d9d787999264aae858b3656b88f7cf8bf0e
Magomed Isaev <critikk99@bk.ru>
2026-04-01 23:24:05 +0300

    refactor(frontend): remove custom MultiSelect component, use PrimeVue MultiSelect

--- apps/frontend/src/components/ui/index.ts
+++ apps/frontend/src/components/ui/index.ts
@@ -9,9 +9,10 @@
 // - Tag: import Tag from 'primevue/tag'
 // - Message: import Message from 'primevue/message'
 // - DataTable/Column: import DataTable from 'primevue/datatable', import Column from 'primevue/column'
+// - MultiSelect: import MultiSelect from 'primevue/multiselect'
+// - DatePicker: import DatePicker from 'primevue/datepicker'
 // - etc.
 
 // Custom UI components that wrap PrimeVue with additional functionality
 export { default as FormGroup } from './FormGroup.vue';
-export { default as MultiSelect } from './MultiSelect.vue';
 export { default as PhoneInput } from './PhoneInput.vue';
