d72e0e07a147bd990c18cfd58e139304485b617c
Magomed Isaev <critikk99@bk.ru>
2026-04-18 13:10:53 +0300

    feat(frontend): add content cards table, commissions/tariffs dialogs, and tariffs view

---
+++ apps/frontend/src/components/content-cards/index.ts
@@ -0,0 +1,3 @@
+export { default as ContentCardsTable } from './ContentCardsTable.vue';
+export { default as CommissionsDialog } from './CommissionsDialog.vue';
+export { default as TariffsDialog } from './TariffsDialog.vue';
